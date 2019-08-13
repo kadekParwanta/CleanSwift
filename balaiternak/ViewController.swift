69ed7c78227319d8091d477e3209a71139c42d37
Kadek Juliana Parwanta <kadek.parwanta@gmail.com>
2019-08-13 09:27:27 +0800

    Initial Commit

---
+++ balaiternak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  balaiternak
+//
+//  Created by Ayu Ratna Wulandari on 13/08/19.
+//  Copyright © 2019 Kadek Juliana Parwanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
